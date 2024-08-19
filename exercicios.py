e63b2a8eae83372e84da95cbe58abb31d203a95f
Fasri <106215404+Fasri@users.noreply.github.com>
2024-08-19 19:45:56 -0300

    Funcionando exercicio5

--- exercicios.py
+++ exercicios.py
@@ -24,11 +24,16 @@
 # for chave, valor in livro1.items():
 #     print(f"{chave}:{valor}")
 # 4.Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
-def contar_caracteres(s):
-    contagem = {}
-    for caractere in s:
-        contagem[caractere] = contagem.get(caractere, 0) + 1
-    return contagem
+# def contar_caracteres(s):
+#     contagem = {}
+#     for caractere in s:
+#         contagem[caractere] = contagem.get(caractere, 0) + 1
+#     return contagem
 
-print(contar_caracteres("engenharia de dados"))
+# print(contar_caracteres("engenharia de dados"))
+
 # 5.Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
+frutas = ["maçã", "banana", "cereja"]
+dici_frutas = {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}
+total = sum(dici_frutas[item] for item in frutas)
+print(f"Preço total: {total}")
