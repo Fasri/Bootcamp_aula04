7885797bda57472633021dd7d700580187df9fb5
Fasri <106215404+Fasri@users.noreply.github.com>
2024-08-19 13:35:11 -0300

    aprendendo dicionarios em python

---
+++ exemplo_dic.py
@@ -0,0 +1,26 @@
+protudo_01 = {
+    "nome" : "Televisão",
+    "quantidade": "34",
+    "preço":20.9,
+    "disponibilidade": True
+}
+
+protudo_02 = {
+    "nome" : "Sapato",
+    "quantidade": "4",
+    "preço":2.9,
+    "disponibilidade": True
+}
+
+protudo_03 = {
+    "nome" : "Memoria",
+    "quantidade": "4",
+    "preço":2.9,
+    "disponibilidade": True
+}
+
+lista_de_produtos = [protudo_01, protudo_02, protudo_03]
+
+print(lista_de_produtos)
+
+print(protudo_01["preço"]+protudo_03["preço"])
