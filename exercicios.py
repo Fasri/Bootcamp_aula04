d094fd048bd218adf12924dcba6e352df4847ff9
Fasri <106215404+Fasri@users.noreply.github.com>
2024-08-19 14:11:52 -0300

    Funcionando exercicio3

--- exercicios.py
+++ exercicios.py
@@ -5,14 +5,23 @@
 #     print(quadrado)
 
 # 2.Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
-linguagem = ["Python", "Java", "C++", "JavaScript"]
-linguagem.remove("C++")
-print(linguagem)
-linguagem.append("Ruby")
-print(linguagem)
+# linguagem = ["Python", "Java", "C++", "JavaScript"]
+# linguagem.remove("C++")
+# print(linguagem)
+# linguagem.append("Ruby")
+# print(linguagem)
 
 
 
 # 3.Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação.
+livro1 ={
+    "Titulo":"Psicologia Financeira",
+    "Autor":"Caleb",
+    "Ano de publicação": 2009
+}
+
+print(livro1)
+for chave, valor in livro1.items():
+    print(f"{chave}:{valor}")
 # 4.Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
 # 5.Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
