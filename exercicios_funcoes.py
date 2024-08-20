fc7c950697a623944e41ab29cd10a0c53ce36cd7
Fasri <106215404+Fasri@users.noreply.github.com>
2024-08-20 14:12:00 -0300

    Funcionando exercicio19

--- exercicios_funcoes.py
+++ exercicios_funcoes.py
@@ -26,11 +26,22 @@
 # 18.Desenvolva uma função que receba uma string como argumento e 
 # retorne essa string revertida.
 
-def reverte_string(original:str):
-    return original[::-1]
+# def reverte_string(original:str):
+#     return original[::-1]
 
-print(reverte_string("teste"))
-print(reverte_string("Gilcilena"))
-# 19.Implemente uma função que receba dois argumentos: uma lista de números e um número. A função deve retornar todas as combinações de pares na lista que somem ao número dado.
+# print(reverte_string("teste"))
+# print(reverte_string("Gilcilena"))
+
+# 19.Implemente uma função que receba dois argumentos: uma lista de números e um número.
+# A função deve retornar todas as combinações de pares na lista que somem ao número dado.
+def fazer_pares(lista:list, numero:int):
+    soma =[]
+    for i in lista:
+        soma.append(i+numero)
+    return soma
+lista1=[4,5,6,7,10]
+numero1 = 9
+print(fazer_pares(lista1, numero1))
+
 
 # 20.Escreva uma função que receba um dicionário e retorne uma lista de chaves ordenadas
