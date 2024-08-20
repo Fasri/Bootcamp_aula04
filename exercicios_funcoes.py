ca0bab61f9d125c643cafc660f150faeaae28ab5
Fasri <106215404+Fasri@users.noreply.github.com>
2024-08-20 13:51:04 -0300

    Funcionando exercicio17

--- exercicios_funcoes.py
+++ exercicios_funcoes.py
@@ -1,16 +1,27 @@
 # 16.Escreva uma função que receba uma lista de números e retorne a soma de todos os números.
-def somar_lista(lista_numeros):
-    soma = 0
-    for numero in lista_numeros:
-        soma = numero + soma
-        
-    return soma
+# def somar_lista(lista_numeros):
+#     soma = 0
+#     for numero in lista_numeros:
+#         soma = numero + soma
+#     return soma
 
-lista = [2,4,5,8]
+# lista = [2,4,5,8]
 
-print(somar_lista(lista))
+# print(somar_lista(lista))
 
-# 17.Crie uma função que receba um número como argumento e retorne True se o número for primo e False caso contrário.
+# 17.Crie uma função que receba um número como argumento e retorne True se o número for primo 
+# e False caso contrário.
+
+def eh_primo(numero:int):
+    if numero <= 1:
+        return False
+    for i in range(2, int(numero ** 0.5) + 1):
+        if numero % i == 0:
+            return False
+    return True
+    
+
+print(eh_primo(17))
 
 # 18.Desenvolva uma função que receba uma string como argumento e retorne essa string revertida.
 
