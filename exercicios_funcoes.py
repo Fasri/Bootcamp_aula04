7799b396f3c1cd330d18b6159ffd3275a0110dd2
Fasri <106215404+Fasri@users.noreply.github.com>
2024-08-20 13:16:37 -0300

    Funcionando exercicio16

---
+++ exercicios_funcoes.py
@@ -0,0 +1,19 @@
+# 16.Escreva uma função que receba uma lista de números e retorne a soma de todos os números.
+def somar_lista(lista_numeros):
+    soma = 0
+    for numero in lista_numeros:
+        soma = numero + soma
+        
+    return soma
+
+lista = [2,4,5,8]
+
+print(somar_lista(lista))
+
+# 17.Crie uma função que receba um número como argumento e retorne True se o número for primo e False caso contrário.
+
+# 18.Desenvolva uma função que receba uma string como argumento e retorne essa string revertida.
+
+# 19.Implemente uma função que receba dois argumentos: uma lista de números e um número. A função deve retornar todas as combinações de pares na lista que somem ao número dado.
+
+# 20.Escreva uma função que receba um dicionário e retorne uma lista de chaves ordenadas
